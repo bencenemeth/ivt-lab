5ea184412f700b0663608171f5d50f5a4650a9b0
bencenemeth <ben23@hotmail.hu>
2018-04-13 04:20:19 -0700

    branch-A modify

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -81,6 +81,8 @@ public class GT4500 implements SpaceShip {
         //TODO implement feature
 	firingSuccess = (primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1));
 	wasPrimaryFiredLast = true;
+
+
         break;
     }
 
